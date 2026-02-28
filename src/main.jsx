af8810bb83bdd0804f30a9a54435951cef398765
mdshoaib77 <email4966@gmail.com>
2026-02-28 19:15:22 +0600

    Alhamdulliah Backend Url Add

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import { BrowserRouter } from "react-router-dom"
-// import ShopContextProvider from './context/ShopContext.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <BrowserRouter>
-//     <ShopContextProvider>
-//       <App />
-//     </ShopContextProvider>
-//   </BrowserRouter>
-// )
-
-
 import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
